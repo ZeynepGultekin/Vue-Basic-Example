51ae10a5cf10a84a96bb91b395066285e1429c3d
ZeynepGultekin <zeynep.pehlivan@windowslive.com>
2021-11-18 16:35:31 +0100

    first vue cli project

---
+++ vue-cli-first-project/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+        <h2>{{friend.name}}</h2>
+        <button @click="toggleDetails"> {{detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{friend.phone}} </li>
+            <li><strong>Email:</strong>{{friend.email}} </li>
+        </ul>
+
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'nur',
+                name:'Nur Pehlivan',
+                phone:'01234 567 890',
+                email: 'nur@localhost.com'
+            },
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+
+};
+</script>
