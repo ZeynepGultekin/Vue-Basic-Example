6969ae3903cd99bf52f2b1f5f26466f850b3a386
ZeynepGultekin <zeynep.pehlivan@windowslive.com>
2021-11-21 15:37:41 +0100

    vue-cli-props

---
+++ vue-cli-props/src/App.vue
@@ -0,0 +1,111 @@
+<template>
+    <section>
+        <header><h1>My Friends</h1></header>
+            <ul>
+                <friend-contact
+                  name="Nur Pehlivann"
+                  phone-number="1234 78992"
+                  email-address="nur@localhost.com"
+                  is-favorite = '1'
+                ></friend-contact>
+                <friend-contact
+                  name="Asel Pehlivan"
+                  phone-number="0987 65431"
+                  email-address="asel@localhost.com"
+                  >
+                </friend-contact>
+            </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends:[
+                {
+                    id: 'nur',
+                    name:'Nur Pehlivan',
+                    phone:'01234 567 890',
+                    email: 'nur@localhost.com'
+                },
+                {
+                    id: 'asel',
+                    name:'Asel Isge',
+                    phone:'9876 543 210',
+                    email: 'asel@localhost.com'
+                },
+
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Jost', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
